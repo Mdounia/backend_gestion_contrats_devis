7bd7f8c82cdfd921b47cfaf87df5a25ded6a96d2
rachid366 <mohamedrachid286@gmail.com>
2022-01-12 18:16:57 +0100

    modifier fournisseur bean

--- src/main/java/ma/gestion/contrat/models/Fournisseur.java
+++ src/main/java/ma/gestion/contrat/models/Fournisseur.java
@@ -1,9 +1,15 @@
 package ma.gestion.contrat.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.List;
-
+@Entity
 public class Fournisseur {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String email;
