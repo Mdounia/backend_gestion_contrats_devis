f1dd66cdd43451ac49d2b7854f58deff6ef7305d
Mdounia <maziane.dounia@gmail.com>
2022-01-12 15:30:46 +0100

    initial project files

---
+++ src/main/java/ma/emsi/projet/GestionContratDevisApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.projet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionContratDevisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionContratDevisApplication.class, args);
+	}
+
+}
